41d7fdc955d53a2170acd5685cf6d4dd059bfaee
GordienkoNadya <noyabrka1983@gmail.com>
2026-02-07 17:01:00 +0300

    Продолжили форму редактирования задачи

--- src/components/EditTaskModal.jsx
+++ src/components/EditTaskModal.jsx
@@ -1,3 +1,7 @@
+import { Form, Input, Modal } from "antd";
+import FormItem from "antd/es/form/FormItem";
+import axios from "axios";
+
 const EditTaskModal = ({ task, visible, onCancel, onUpdate }) => {
     const [form] = Form.useForm();
     //два состояния для хранения списка проектов и состояния загрузки
@@ -7,25 +11,81 @@ const EditTaskModal = ({ task, visible, onCancel, onUpdate }) => {
     const [tags, setTags] = useState([]);
 
     useEffect(() => {
-        if (visible){
+        if (visible) {
             axios.get('http://127.0.0.1:8000/api/projects/') //гет запрос к апи проектов
-            .then(response => {
-                setProjects(response.data) //сохраняем проекты в состоянии
-            })
-            .catch(error => {
-                console.error("Ошибка при загрузке проектов", error);
-                message.error("Не удалось загрузить проекты");
-            });
-        axios.get('http://127.0.0.1:8000/api/tags/') //гет запрос к апи проектов
-            .then(response => {
-                setTags(response.data) //сохраняем проекты в состоянии
-            })
-            .catch(error => {
-                console.error("Ошибка при загрузке тегов", error);
-                message.error("Не удалось загрузить теги");
+                .then(response => {
+                    setProjects(response.data) //сохраняем проекты в состоянии
+                })
+                .catch(error => {
+                    console.error("Ошибка при загрузке проектов", error);
+                    message.error("Не удалось загрузить проекты");
+                });
+            axios.get('http://127.0.0.1:8000/api/tags/') //гет запрос к апи проектов
+                .then(response => {
+                    setTags(response.data) //сохраняем проекты в состоянии
+                })
+                .catch(error => {
+                    console.error("Ошибка при загрузке тегов", error);
+                    message.error("Не удалось загрузить теги");
+                });
+        }
+
+    }, [visible]);
+    //заполнить форму данными задачи при открытия
+    useEffect(() => {
+        if (task && visible) {
+            form.setFieldsValue({
+                title: task.title,
+                description: task.description,
+                due_date: task.due_date,
+                project: task.project.id,
+                tags: task.tags?.map(tag => tag.id) || [],
+                is_completed: task.is_completed
             });
         }
-        
-    }, [visible]);
-//заполнить форму данными задачи при открытии остановились
+    }, [task, visible, form]);
+    //функция сохранения изменений
+    const handleSave = (values) => {
+        setLoading(true);
+
+        const updateTask = {
+            ...values,
+            due_date: values.due_date.format("YYYY-MM-DD HH:mm:ss"),
+            tags: values.tags || []
+        };
+
+        axios.put(`http://127.0.0.1:8000/api/tasks/${task.id}/`, updateTask)
+            .then(response => {
+                onUpdate(response.data);
+            })
+            .catch(error => {
+                console.error("Ошибка при обновлении задачи", error);
+                message.error("Не удалось обновить задачу");
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+
+    return (
+        <Modal
+            title="Редактировать задачу"
+            open={visible}
+            onCancel={onCancel}
+            width={700}
+        >
+            <Form
+                form={form}
+                layout="vertical"
+                onFinish={handleSave}>
+                <Form.Item
+                    label="Название задачи"
+                    name="title"
+                    rules={[{ required: true, message: "Введите название задачи" }]}
+                >
+                    <Input placeholder="Название задачи"></Input> //продолжить писать элемент дискрипшен
+                </Form.Item>
+            </Form>
+        </Modal>
+    )
 }
